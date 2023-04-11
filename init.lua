b01b22bb2f33721dd80034825ae4eb67cd3c1d42
Carlos Coves Prieto <escorponox@gmail.com>
2023-04-11 12:13:23 +0200

    lazy

--- init.lua
+++ init.lua
@@ -1,65 +1,21 @@
--- Install packer
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-  vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- plugins
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+require('keymaps')
 
-  use 'gruvbox-community/gruvbox'
-  use { "catppuccin/nvim", as = "catppuccin" }
-  -- use "rebelot/kanagawa.nvim"
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-  }
-
-  use {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
-
-  use {
-    'nvim-treesitter/nvim-treesitter-context',
-    after = 'nvim-treesitter',
-  }
-
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
-  --use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-  use { 'nvim-telescope/telescope-fzf-native.nvim',
-    run =
-    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-  use 'ilAYAli/scMRU.nvim'
-
-  use 'mbbill/undotree'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-commentary'
-  use 'towolf/vim-helm'
-
-  use 'nvim-lualine/lualine.nvim'
-  use 'windwp/nvim-autopairs'
-
-  use {
-    'kyazdani42/nvim-tree.lua',
-  }
-  use 'github/copilot.vim'
-
-  use { 'neoclide/coc.nvim', branch = 'release' }
-  use 'escorponox/telescope-coc.nvim'
-
-  use 'rcarriga/nvim-notify'
-end)
+require('lazy').setup('plugins')
 
 require('autocommands')
-require('keymaps')
 require('commands')
 
 require('telescope-config')
